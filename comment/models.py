9aca6c2dc19e174449bc7872a14d148e0bd0e866
yehoon <kimyehoon74@gmail.com>
2023-08-18 20:35:52 +0900

    comment and post

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from post.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
+    content = models.TextField(default="Your Default Value Here")
+    def __str__(self):
+        return f"Comment to '{self.post.title}'"
+    
+    id = models.AutoField(primary_key=True)
--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Post, Comment
 
-# Create your views here.
-def comment(request):
-    return render(request, 'comment.html')
+@csrf_exempt
+def add_comment(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+
+        if request.method == "POST":
+            data = json.loads(request.body.decode('utf-8'))
+            content = data['content']
+
+            # Create a new comment object and associate it with the post
+            comment = Comment(post=post, content=content)
+            comment.save()
+
+            return JsonResponse({'message': 'Comment added successfully'})
+        else:
+            return JsonResponse({'error': 'Method not allowed'}, status=405)
+    except Post.DoesNotExist:
+        return JsonResponse({'error': 'Post not found'}, status=404)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    except KeyError as e:
+        return JsonResponse({'error': f'Missing key: {str(e)}'}, status=400)
+
+def get_comment_detail_as_json(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+        comments = Comment.objects.filter(post=post)
+        comments_json = [
+            {
+                'id': comment.id,
+                'content': comment.content,
+            }
+            for comment in comments
+        ]
+        return JsonResponse({'comments': comments_json})
+    except Post.DoesNotExist:
+        return JsonResponse({'error': 'Post not found'}, status=404)
